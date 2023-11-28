12221265c6d33e9c06d96511f96b7a33bf0f476d
alexander.kovalchuk <alexander.kovalchuk.aka.maki@gmail.com>
2023-11-29 01:46:55 +0300

    done

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Материалы
+
+[Числительные с существительные](https://www.urya.ru/catalog/71-chislitelnye/82-tablica_chislitelnye_s_suschestvitelnymi.html)  
+[Циклы for, while, do-while, forEach, repeat()](https://developer.alexanderklimov.ru/android/kotlin/for.php)  
+
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main() {
+
+    var like = 0
+
+    /**
+     * подготовка текста по правилам русского языка
+     */
+    fun makeLike(): String {
+        like += 1
+        return if ((like - 1) % 10 == 0) "$like человеку" else "$like людям"
+    }
+
+    // отладка
+    for (i in 1..1000){
+        println("Понравилось ${makeLike()}")
+    }
+}
+
