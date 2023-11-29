c93aac2a2d608f01c70a5e2da4df60c7a3b1ffcc
alexander.kovalchuk <alexander.kovalchuk.aka.maki@gmail.com>
2023-11-30 00:42:01 +0300

    fix for 11

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,11 +7,11 @@ fun main() {
      */
     fun makeLike(): String {
         like += 1
-        return if ((like - 1) % 10 == 0) "$like человеку" else "$like людям"
+        return if ((like - 1) % 10 == 0 && (like - 11) % 100 != 0) "$like человеку" else "$like людям"
     }
 
     // отладка
-    for (i in 1..1000){
+    for (i in 1..1000) {
         println("Понравилось ${makeLike()}")
     }
 }
